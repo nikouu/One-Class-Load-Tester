24e27ae38b8f311dc9af8cc78b8fcd27a1f61323
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2023-01-20 15:47:10 +1300

    Added random delay to server

--- OneClassLoadTester.TestServer/Program.cs
+++ OneClassLoadTester.TestServer/Program.cs
@@ -2,9 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/test", () =>
+app.MapGet("/test", async () =>
 {
     Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Request recieved.");
+    await Task.Delay(Random.Shared.Next(100, 1000));
 });
 
 app.Run("http://localhost:3000");

1d218a37d7b1e9ede5a30109eb4be458f052aad8
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2023-01-20 16:07:04 +1300

    Rewritten to use async completely and to move the logic into an actual class

---
+++ OneClassLoadTester/LoadTestEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneClassLoadTester
+{
+    /// <summary>
+    /// Works by sending x number of requests then a defined delay, and onto the next group of requests.
+    /// </summary>
+    public class LoadTestEngine
+    {
+        public async Task Run()
+        {
+            var totalTestGroups = 100;
+            var totalTestTasks = 50;
+            var httpClient = new HttpClient();
+
+            // This represents all the different cycles of requests 
+            var testGroups = new List<TestGroup>();
+
+            // setup test data
+            for (int testGroupCount = 0; testGroupCount < totalTestGroups; testGroupCount++)
+            {
+                var testTasks = new List<Task>();
+                for (int testTaskCount = 0; testTaskCount < totalTestTasks; testTaskCount++)
+                {
+                    // Be extremely careful, this is a COLD task
+                    // This is where the work happens, in this lambda 👇
+                    var task = new Task(async () =>
+                    {
+                        _ = await httpClient.GetAsync("http://localhost:3000/test");
+                        Console.WriteLine("Finished task.");
+                    });
+                    testTasks.Add(task);
+                }
+
+                var testGroup = new TestGroup(testTasks, Random.Shared.Next(1, 500));
+                testGroups.Add(testGroup);
+            }
+
+            foreach (var testgroup in testGroups)
+            {
+                // Fires off all the test tasks in a test group.
+                await Task.WhenAll(testgroup.TestTasks.Select(task =>
+                {
+                    Console.WriteLine($"Starting task.");
+                    task.Start();
+                    return task;
+                }));
+
+                await Task.Delay(testgroup.PostTestDelay);
+            }
+        }
+    }
+
+    public record TestGroup(List<Task> TestTasks, int PostTestDelay);
+}
--- OneClassLoadTester/Program.cs
+++ OneClassLoadTester/Program.cs
@@ -1,34 +1,10 @@
-﻿Console.WriteLine("Starting load test.");
+﻿using OneClassLoadTester;
 
-var httpClient = new HttpClient();
+Console.WriteLine("Starting load test.");
 
-var requestsPerCycleMapping = new List<CycleData>()
-{
-    new CycleData(1, 5, 1000),
-    new CycleData(3, 10, 500),
-    new CycleData(5, 15, 300),
-    new CycleData(8, 20, 100),
-    new CycleData(10, 25, 1000),
-}.OrderBy(x => x.Cycle);
+var loadTestEngine = new LoadTestEngine();
 
-var cycles = requestsPerCycleMapping.Max(x => x.Cycle);
-
-for (int i = 0; i < cycles; i++)
-{
-    var cycleData = GetCurrentCycleData(i);
-    Console.WriteLine($"Cycle {i + 1}: sending {cycleData.Requests} requests, then delaying by {cycleData.Delay}ms.");
-
-    for (int j = 0; j < cycleData.Requests; j++)
-    {
-        _ = await httpClient.GetAsync("http://localhost:3000/test");
-    }
-
-    await Task.Delay(cycleData.Delay);
-}
+await loadTestEngine.Run();
 
 Console.WriteLine("Load test complete.");
 Console.ReadLine();
-
-CycleData GetCurrentCycleData(int currentCycle) => requestsPerCycleMapping.First(x => x.Cycle >= currentCycle);
-
-readonly record struct CycleData(int Cycle, int Requests, int Delay);
